553d3dde93590aec4092bcc2f0bdfbc38f1b7db5
Antonio Cervone <ant.cervone@gmail.com>
2015-07-24 15:01:47 +0200

    add time support to data

--- data.py
+++ data.py
@@ -7,6 +7,8 @@ class Data:
     def __init__(self, **config):
         print 'data configuration:', config
         self.config = config
+        self.grid = []
+        self.times = []
 
     def read(self):
         f = open(self.config['fileName'])
@@ -17,48 +19,53 @@ class Data:
         print 'numPoints =', numPts
 
         # second line is number of timesteps
-        numTimes = int(f.readline().split()[2])
-        print 'numTimes =', numTimes
+        self.numTimes = int(f.readline().split()[2])
+        print 'numTimes =', self.numTimes
 
         # third line used for var names
         titles = f.readline().split()
 
         # ignore '#', 'x', 'y', 'z', 't'
         numVars = len(titles)-5
-        varList = []
+        varNames = []
         for v in range(0, numVars):
-            name = titles[5+v]
-            print 'creating array', name
-            var = vtk.vtkFloatArray()
-            var.SetName(name)
-            var.SetNumberOfValues(numPts)
-            varList.append(var)
+            varNames.append(titles[5+v])
 
-        pts = vtk.vtkPoints()
-        pts.SetNumberOfPoints(numPts)
-        for i in xrange(0, numPts):
-            words = f.readline().split()
-            p = [float(words[0]), float(words[1]), float(words[2])]
-            pts.SetPoint(i, p)
+        for t in xrange(0, self.numTimes):
+            varList = []
             for v in range(0, numVars):
-                varList[v].SetValue(i, float(words[4+v]))
+                print 'creating array', varNames[v]
+                var = vtk.vtkFloatArray()
+                var.SetName(varNames[v])
+                var.SetNumberOfValues(numPts)
+                varList.append(var)
 
-        # delaunay
-        profile = vtk.vtkPolyData()
-        profile.SetPoints(pts)
-        for v in range(0, numVars):
-            profile.GetPointData().AddArray(varList[v])
-        profile.GetPointData().SetActiveScalars(varList[0].GetName())
+            pts = vtk.vtkPoints()
+            pts.SetNumberOfPoints(numPts)
+            for i in xrange(0, numPts):
+                words = f.readline().split()
+                p = [float(words[0]), float(words[1]), float(words[2])]
+                time = float(words[3])
+                pts.SetPoint(i, p)
+                for v in range(0, numVars):
+                    varList[v].SetValue(i, float(words[4+v]))
+            self.times.append(time)
 
-        self.grid = vtk.vtkDelaunay3D()
-        self.grid.SetInputData(profile)
-        self.grid.SetTolerance(0.01)
-        self.grid.SetAlpha(0.0)
-        self.grid.BoundingTriangulationOff()
+            # delaunay
+            profile = vtk.vtkPolyData()
+            profile.SetPoints(pts)
+            for v in range(0, numVars):
+                profile.GetPointData().AddArray(varList[v])
+            profile.GetPointData().SetActiveScalars(varList[0].GetName())
 
+            self.grid.append(vtk.vtkDelaunay3D())
+            self.grid[t].SetInputData(profile)
+            self.grid[t].SetTolerance(0.01)
+            self.grid[t].SetAlpha(0.0)
+            self.grid[t].BoundingTriangulationOff()
 
     def getVarList(self):
-        pointData = self.grid.GetInput().GetPointData()
+        pointData = self.grid[0].GetInput().GetPointData()
         numVars = pointData.GetNumberOfComponents()
         varNames = []
         for i in range(0, numVars):
@@ -67,16 +74,21 @@ class Data:
         return varNames
 
     def write(self, fileName):
+        if fileName[-4:] == '.vtu':
+            fileName = fileName[:-4]
         writer = vtk.vtkXMLDataSetWriter()
-        writer.SetInputConnection(self.grid.GetOutputPort())
-        writer.SetFileName(fileName)
         writer.SetDataModeToAscii()
-        writer.Write()
+        for t in xrange(0, self.numTimes):
+            fileNameT = '{:s}.{:03d}.vtu'.format(fileName, t)
+            print "printing {:s} ...".format(fileNameT)
+            writer.SetInputConnection(self.grid[t].GetOutputPort())
+            writer.SetFileName(fileNameT)
+            writer.Write()
 
 if __name__ == '__main__':
     from config import *
 
-    data = Data(fileName='test_notime.dat')
+    data = Data(fileName='test.dat')
 
     data.read()
 
