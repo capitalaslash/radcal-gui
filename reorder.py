c4213f4a418485b749027e66708d60eff8848d9c
Antonio Cervone <ant.cervone@gmail.com>
2015-12-16 11:42:59 +0100

    refactor and improve the reorder capabilities

--- reorder.py
+++ reorder.py
@@ -1,7 +1,20 @@
 import numpy as np
 
-def reorder_3d(**kwargs):
-    """Reorder a RADCAL-III air output file so that all variables are ascending"""
+def reorder(data, index, maxindex):
+    """Reorder numpy array data starting form index and up to maxindex columns."""
+    data = data[data[:,index].argsort()]
+    index += 1
+    if index < maxindex:
+        coord, indices = np.unique(data[:,index-1], return_index=True)
+        indices = np.append(indices, data.shape[0])
+        for i in range(0, coord.shape[0]):
+            sub = data[indices[i]:indices[i+1]]
+            sub = reorder(sub, index, maxindex)
+            data[indices[i]:indices[i+1]] = sub
+    return data
+
+def reorder_file(**kwargs):
+    """Reorder a RADCAL-III output file so that all variables are ascending."""
 
     if not 'filename_in' in kwargs:
         "no input filename provided. aborting"
@@ -15,23 +28,28 @@ def reorder_3d(**kwargs):
 
     data = np.genfromtxt(filename_in, delimiter=',')
 
-    data = data[data[:,0].argsort()]
-    coord_x, indices_x = np.unique(data[:,0], return_index=True)
-    indices_x = np.append(indices_x, data.shape[0])
+    if not 'maxindex' in kwargs:
+        maxindex = data.shape[1]
 
-    for i in range(0, coord_x.shape[0]):
-        plane = data[indices_x[i]:indices_x[i+1]]
-        coord_y, indices_y = np.unique(plane[:,1], return_index=True)
-        indices_y = np.append(indices_y, plane.shape[0])
-        plane = plane[plane[:,1].argsort()]
-        for j in range(0, coord_y.shape[0]):
-            line = plane[indices_y[j]:indices_y[j+1]]
-            line = line[line[:,2].argsort()]
-            plane[indices_y[j]:indices_y[j+1]] = line
-
-        data[indices_x[i]:indices_x[i+1]] = plane
+    data = reorder(data, 0, maxindex)
 
     np.savetxt(filename_out, data, delimiter=', ')
+
+def reorder_test():
+    """Unit test for the reorder function."""
+    data = np.array([
+        [1, 1, 1], [1, 1, 2], [1, 2, 1], [1, 2, 2],
+        [2, 1, 1], [2, 1, 2], [2, 1, 3], [2, 2, 1], [2, 2, 2], [2, 2, 3],
+        [3, 1, 1], [3, 1, 2], [3, 1, 3], [3, 2, 1], [3, 2, 2], [3, 2, 3]
+        ])
+    data_orig = np.copy(data)
+
+    np.random.shuffle(data)
+    # print data
+    # np.savetxt('test_reorder.dat', data, delimiter=', ')
+
+    data = reorder(data, 0, 3)
+    print "reordered: ", (data == data_orig).all()
 
 if __name__ == '__main__':
     import sys
@@ -44,4 +62,4 @@ if __name__ == '__main__':
     basename, ext = os.path.splitext(infile)
     outfile = basename + '_ordered' + ext
 
-    reorder_3d(filename_in=infile, filename_out=outfile)
+    reorder_file(filename_in=infile, filename_out=outfile)
